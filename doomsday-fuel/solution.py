c0700d59679cfa80533c07b11b303681d04e359e
Amod Mulay <AmodMulay1@gmail.com>
2019-06-20 19:03:24 -0400

    last ditch try

--- doomsday-fuel/solution.py
+++ doomsday-fuel/solution.py
@@ -5,154 +5,136 @@ except ImportError:
     from fractions import gcd
     from Queue import Queue
 from collections import defaultdict
-from fractions import Fraction
+from fractions import Fraction as F
 from copy import deepcopy
 
 
 DEBUG = False
-#DEBUG = True
+DEBUG = True
 
-def printd(s):
+def printd(s, delay=0):
     if DEBUG:
         print(s)
+        from time import sleep
+        sleep(delay)
 
 def is_loop(chain, state, offset=0):
-    for element in chain[:len(chain)+offset]:
-        if element[0] == state:
-            return True
-    return False
+    return state in chain[:len(chain)+offset]
 
-def get_state_transitions(m):
-    terminal_transitions = defaultdict(lambda: list(), {})
-    looped_transitions = defaultdict(lambda: list(), {})
-    parents = defaultdict(lambda: list(), {})
+def normalize_loops(loops, m):
+    # here we will rotate loops so least state is the first element
+    # then we will remove repeated loops
     n_states = len(m)
-    queue = Queue()
-    visited_states = {}
-    for i in range(len(m)):
-        visited_states[i] = False
-
-    queue.put([(0, Fraction(1))])  # put state id , prior probability
-    while not queue.empty():
-        element = queue.get()
-        state = element[-1][0]
-        prior_prob = element[-1][1]
-
-        denominator = sum(m[state])
-        if  visited_states[state] and is_loop(element, state, offset=-1):  # looped transition chain
-            #printd("loop {}".format(element))
-            prior_prob = 1
-            while element[0][0] != state:
-                temp = element.pop(0)
-            #    prior_prob *= temp[1]
-            #element[0] = (element[0][0], element[0][1]/prior_prob)
-            looped_transitions[state].append(element)
-        else:
-            if not element in terminal_transitions[state]:
-                terminal_transitions[state].append(element)
-                if state != 0 and not element[-2][0] in parents[state]:
-                    parents[state].append(element[-2][0])  # store parent
-
-            visited_states[state] = True
-            for i in range(len(m[state])):
-                if m[state][i] != 0:
-                    new_elem = deepcopy(element)
-                    new_elem.append((i, Fraction(m[state][i], denominator)))
-                    queue.put(new_elem)
-
-    # We need to normalize the loops and remove duplicates
-    # to do that we first pop last element to remove duplicate
-    # then we rotate the loop so the min state value is the first node
-    loops = []
-    for i in looped_transitions.keys():
-        chains = looped_transitions[i]
-        #printd(transitions[i])
-        for j in range(len(chains)):
-            chain = chains[j]
-            chain.pop(0)
-            li = get_least_index(chain)
-            while chain[0][0] != li:
-                temp = chain.pop(0)
-                chain.append(temp)
-            if chain not in loops:
-                loops.append(chain)
-    printd("normalized loops: {}".format(loops))
-
-    #recalculate transition fractions for loops
+    norm_loops = []
     for loop in loops:
-        for i in range(len(loop)):
-            s0 = loop[i][0]
-            s1 = loop[(i+1)%len(loop)][0]
-            fr = Fraction(m[s0][s1],sum(m[s0]))
-            loop[i] = (s0, fr)
+        min_state = min(loop)
+        while loop[0] != min_state:
+            state = loop.pop(0)
+            loop.append(state)
+        if not loop in norm_loops:
+            norm_loops.append(loop)
+    return norm_loops
 
-    printd("terminal_transitions: {}".format(terminal_transitions))
-    printd("looped_transitions: {}".format(loops))
-    return (terminal_transitions, loops, parents)
-
-
-def eval_terminal_probabilities(transitions, n_states):
-    probs = [0] * n_states
-
-    for i in range(n_states):
-        #aggj = Fraction(0)
-        chains = transitions[i]
-        #printd(transitions[i])
-        for j in range(len(chains)):
-            chain = chains[j]
-            aggk = Fraction(1)
-            for k in range(1, len(chain)):
-                node = chain[k]
-                aggk *= node[1]
-            printd("state: {}, aggk: {}".format(i, aggk))
-            probs[i] += aggk
-    return probs
-
-def get_least_index(chain):
-    li = chain[0][0]
-    for node in chain:
-        li = min(li,node[0])
-    return li
-
-def eval_looped_probabilities(loops, parents,
-        m, prior_probs):
+def traverse_m(m):  # assumes m has been converted to fractions
+    paths = []
+    path = [0]
+    loops = []
     n_states = len(m)
-    # loop_factor[i] = % or orig. value that comes back after one loop
-    # for each state summed over all loops that state is a part of
-    loop_factor = [0] * n_states
-    for j in range(len(loops)):
-        chain = loops[j]
-        aggk = Fraction(1)
-        for k in range(len(chain)):
-            aggk *= chain[k][1]
-        #print(transitions[i][j])
-        aggk
-        # aggk now represents the loop_factor for current loop
-        # add aggk to each loop element's total
-        # skip chain[0] since chain[0] = chain[-1]
-        for k in range(len(chain)):
-            loop_factor[chain[k][0]] += aggk
-    printd("loop_factor: {}".format(loop_factor))
+    visited_states = [False]*n_states
+    parents = defaultdict(lambda: list(), {})
+    queue = Queue()
+    queue.put(path)  # we always start with state 0
+    path_tree = defaultdict(lambda: list(), {})  # parent: list of children
+    lin_vals = [0]*n_states
+    while not queue.empty():
+        path = queue.get()
+        state = path[-1]
+        if visited_states[state] and is_loop(path, state, -1):
+            # This is a loop
+            path.pop()
+            while path[0] != state:
+                path.pop(0)
+            loops.append(path)
+        else:
+            # not a loop get children and continue
+            visited_states[state] = True
+            if sum(m[state]) == 0:
+                # terminal state
+                paths.append(path)
+                parents[state].append(path[-2])
+            else:
+                #printd("c3: path: {}".format(path))
+                if state == 0:
+                    lin_vals[0] = 1
+                else:
+                    lin_vals[state] += lin_vals[path[-2]]*m[path[-2]][state]
+                for i in range(n_states):
+                    if m[state][i] != 0:
+                        path_tree[state].append(i)
+                        new_path = deepcopy(path)
+                        new_path.append(i)
+                        queue.put(new_path)
+    printd("paths: {}".format(paths))
+    path_tree = dict(path_tree)
+    printd("path_tree: {}".format(path_tree))
 
-    agg_loop_factor = [0] * n_states
+    printd("loops: {}".format(loops))
+    parents = dict(parents)
+    printd("parents: {}".format(parents))
 
-    for i in range(n_states):
-        agg_loop_factor[i] = Fraction(1) / (1 - loop_factor[i])
+    norm_loops = normalize_loops(loops, m) 
+    printd("norm_loops: {}".format(norm_loops))
 
-    printd("agg_loop_factor: {}".format(agg_loop_factor))
 
-    term_node_agg = [0] * n_states
-    # now we need to calc sum of loop_factors*prior_probs*m[parent][term_node]
-    # for terminal parent as a sum of the loop_factors of each parent
-    for i in parents.keys():
-        for ps in parents[i]:
-            term_node_agg[i] += agg_loop_factor[ps] \
-                                * prior_probs[ps] \
-                                * Fraction(m[ps][i], sum(m[ps]))
+    return (paths, norm_loops, parents, lin_vals)
 
-    printd("term_node_agg: {}".format(term_node_agg))
+def calc_linear_values(path_tree, m):
+    n_states = len(m)
+    lin_vals = [0]*n_states
+    lin_vals[0] = 1
+    for i in range(len(m)):
+        agg = 1
+        for i in range(1, len(path)):
+            ps = path[i-1]
+            cs = path[i]
+            agg *= m[ps][cs]
+            lin_vals[cs] += agg
+    return lin_vals
 
-    return term_node_agg
+def calc_loop_gains(loops, m):
+    loop_iter = [0]*len(m)
+    printd("input loops: {}".format(loops))
+    printd("input m: {}".format(m))
+    for loop in loops:
+        agg = 1
+        len_loop = len(loop)
+        for i in range(len_loop):
+            cs = loop[i]               # current state
+            ns = loop[(i+1)%len_loop]  # parent state
+            #printd("cs:{} ns:{} m[cs][ns]:{}".format(cs, ns, m[cs][ns]))
+            agg *= m[cs][ns]
+ 
+        for state in loop:
+            loop_iter[state] += agg
+
+    printd("loop_iter: {}".format(loop_iter))
+
+    loop_gains = [F(1)/(1-i) for i in loop_iter]
+    printd("loop_gains: {}".format(loop_gains))
+    return loop_gains
+
+def calc_final_states(parents, m, linear_vals, loop_gains):
+    final_states = [0] * len(m)
+    for term_state in parents.keys():
+        for parent in parents[term_state]:
+            printd("linear_val:{} loop_gain:{} m_val: {}".format(linear_vals[parent], loop_gains[parent], m[parent][term_state]))
+            prod = linear_vals[parent] \
+                    * loop_gains[parent] \
+                    * m[parent][term_state]
+            printd("parent:{} term_state:{} prod:{}".format(parent, 
+                term_state, prod))
+            final_states[term_state] += prod
+    return final_states
 
 def lcm(a, b):
     if a==0 and b==0:
@@ -167,27 +149,32 @@ def get_lcm(fracs):
     return base
 
 def solution(m):
-    transitions = get_state_transitions(m)
-    prior_probs = eval_terminal_probabilities(transitions[0], len(m))
-    printd("terminal_probs: {}".format(prior_probs))
-    printd("parents: {}".format(transitions[2]))
+    mf = deepcopy(m)
+    for row in mf:
+        sum_r = sum(row)
+        if sum_r != 0:
+            for i in range(len(row)):
+                row[i] = F(row[i], sum_r)  # convert m to fractions
 
-    final_fractions = eval_looped_probabilities(transitions[1],
-            transitions[2], m, prior_probs)
-    printd("final_fractios: {}".format(final_fractions))
+    (paths, loops, parents, linear_vals) = traverse_m(mf)
 
+    #linear_vals = calc_linear_values(paths, mf)
+    printd("linear_vals: {}".format(linear_vals))
+    loop_gains = calc_loop_gains(loops, mf)
+    final_states = calc_final_states(parents, mf, linear_vals, loop_gains)
     # format output
     denominators = [sum(i) for i in m]
     stable_states = [i for i in range(len(denominators))
             if denominators[i] == 0]
 
+    final_fractions = final_states
+    # normalize final_fractions
+    total = sum(final_fractions)
+    final_fractions = [i/total for i in final_fractions]
 
-    # Zero out non terminal remainders
-    for i in range(len(m)):
-        if not i in stable_states:
-            final_fractions[i] = 0
     lcm = get_lcm(final_fractions)
 
+    printd("final_fractions: {}".format(final_fractions))
     printd("lcm: {}".format(lcm))
     ret_val = []
     den = 0
@@ -196,6 +183,4 @@ def solution(m):
         ret_val.append(int(final_fractions[i]*lcm))
     ret_val.append(lcm)
     return ret_val
-
-
 
